6f8df18eab71392fd49bdddf16763714a503dee2
andynaguyen <andylikestonguyen@gmail.com>
2019-10-15 07:57:01 -0700

    Simplified storage

--- src/storage.js
+++ src/storage.js
@@ -1,34 +1,26 @@
 export default class {
   constructor(sync = true) {
-    this.storeKey = 'PostoryTrackingInfo';
     this.storageArea = sync ? chrome.storage.sync : chrome.storage.local;
   }
 
   add(trackingInfo) {
     const key = `${trackingInfo.carrier}#${trackingInfo.tracking_number}`;
-    this.storageArea.get(this.storeKey, (result) => {
-      const updated = { ...result[this.storeKey], ...{ [key]: trackingInfo } };
-      this.storageArea.set({ [this.storeKey]: updated });
-    });
+    this.storageArea.set({ [key]: trackingInfo });
   }
 
   get(key, callback) {
-    this.storageArea.get(this.storeKey, (result) => callback(result[this.storeKey][key]));
+    this.storageArea.get(key, (result) => callback(result));
   }
 
   getAll(callback) {
-    this.storageArea.get(this.storeKey, (result) => {
-      const entries = Object.values(result[this.storeKey] || {});
+    this.storageArea.get(null, (result) => {
+      const entries = Object.values(result || {});
       callback(entries);
     });
   }
 
   remove(key) {
-    this.storageArea.get(this.storeKey, (result) => {
-      const updated = { ...result[this.storeKey] };
-      delete updated[key];
-      this.storageArea.set({ [this.storeKey]: updated });
-    });
+    this.storageArea.remove(key);
   }
 
   update() {}
