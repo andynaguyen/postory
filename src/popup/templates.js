3ad2f5c65a33c49fa060b23a90e383485697c25b
andynaguyen <andylikestonguyen@gmail.com>
2019-10-17 22:32:20 -0700

    Add icons and remove inline style

--- src/popup/templates.js
+++ src/popup/templates.js
@@ -1,15 +1,15 @@
 import { DateTime } from 'luxon';
 
-export const TrackingInfoList = (trackingInfos) =>
-  `
-    <ul id="tracking-info-list" style="list-style: none;padding-bottom: 3%">
-      ${trackingInfos.map((trackingInfo) => TrackingInfoListItem(trackingInfo)).join('')}
-    </ul>
-  `;
+const ImgFileByCarrier = {
+  fedex: 'images/fedex.webp',
+  dhl_express: 'images/dhl.webp',
+  ups: 'images/ups.webp',
+  usps: 'images/usps.png',
+};
 
 const TrackingInfoListItem = (trackingInfo) =>
   `
-    <li id="${`${trackingInfo.carrier}#${trackingInfo.tracking_number}`}" class="container tracking-info" style="padding-top:3%;padding-left:3%;padding-right:3%;">
+    <li id="${`${trackingInfo.carrier}#${trackingInfo.tracking_number}`}" class="container tracking-info" >
       <div class="card">
         <div class="card-header">
           <p class="card-header-title">${trackingInfo.tracking_number}</p>
@@ -18,10 +18,25 @@ const TrackingInfoListItem = (trackingInfo) =>
           </div>
         </div>
         <div class="card-content">
-          <p>CARRIER: ${trackingInfo.carrier}</p>
-          <p>ETA: ${DateTime.fromISO(trackingInfo.eta).toLocaleString(DateTime.DATETIME_MED)}</p>
-          <p>STATUS: ${trackingInfo.tracking_status.status}</p>
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img src="${ImgFileByCarrier[trackingInfo.carrier]}" alt="${trackingInfo.carrier}"/>
+              </figure>
+            </div>
+            <div class="media-content tracking-info-media-content" >
+              <p>ETA: ${DateTime.fromISO(trackingInfo.eta).toLocaleString(DateTime.DATETIME_MED)}</p>
+              <p>STATUS: ${trackingInfo.tracking_status.status}</p>
+            </div>
+          </div>
         </div>
       </div>
     </li>
+  `;
+
+export const TrackingInfoList = (trackingInfos) =>
+  `
+    <ul id="tracking-info-list">
+      ${trackingInfos.map((trackingInfo) => TrackingInfoListItem(trackingInfo)).join('')}
+    </ul>
   `;
